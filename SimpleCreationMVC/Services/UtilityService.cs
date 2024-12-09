d81cdc3e5f6c482aedb83c158ed784c6a6ab993f
CeejayAlcoba <smithkim909@gmail.com>
2024-12-09 15:23:40 +0800

    update namspace

--- SimpleCreationMVC/Services/UtilityService.cs
+++ SimpleCreationMVC/Services/UtilityService.cs
@@ -8,41 +8,47 @@ namespace SimpleCreationMVC.Services
         public FileService _fileService = new FileService();
         public void CreateAutoMapperConfigFile()
         {
-            var text = @"
+            var text = $@"
 using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Repositories.Utilities
-{
+namespace Project.{FolderNames.Utilities}
+{{
     public class AutoMapperConfig
-    {
+    {{
         public TDestination Map<TDestination>(object source)
-        {
+        {{
             var config = new MapperConfiguration(cfg =>
-            {
+            {{
+                // Dynamically create the map based on the source and destination types
                 cfg.CreateMap(source.GetType(), typeof(TDestination));
-            });
+            }});
             var mapper = new Mapper(config);
             return mapper.Map<TDestination>(source);
-        }
+        }}
+
         public List<TDestination> MapList<TDestination>(IEnumerable<object> source)
-        {
+        {{
             if (source == null || !source.Any())
-            {
+            {{
                 return new List<TDestination>();
-            }
+            }}
             var sourceType = source.First().GetType();
 
             var config = new MapperConfiguration(cfg =>
-            {
+            {{
+                // Dynamically create the map based on the source and destination types
                 cfg.CreateMap(sourceType, typeof(TDestination));
-            });
+            }});
             var mapper = new Mapper(config);
 
             // Perform mapping
             return mapper.Map<List<TDestination>>(source);
-        }
-    }
-}
+        }}
+    }}
+}}
 ";
             _fileService.Create(FolderNames.Utilities.ToString(), "AutoMapperConfig.cs", text);
         }
